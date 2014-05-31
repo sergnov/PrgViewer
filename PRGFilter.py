1342f6a381fcb9b822dc60914c5a5311f2459feb
S0nov <zkgoldenman@gmail.com>
2014-05-31 13:22:14 +0400

    New Components
    new components model

---
+++ PRGFilter.py
@@ -0,0 +1,10 @@
+from PRGComponent import PRGComponent
+
+class PRGFilter(PRGComponent):
+    pass
+    
+if __name__=="__main__":
+    from tkinter import Tk
+    root = Tk()
+    frm = PRGFilter(root)
+    root.mainloop()
