740cceda0a5f7b8cf17a7394ef181f1e6f3696f7
S0nov <zkgoldenman@gmail.com>
2014-05-31 13:30:01 +0400

    Move to tag
    Simplifying job to canvas, when go to tag.

--- prgviewer.py
+++ prgviewer.py
@@ -1,16 +1,19 @@
 #__encoding=utf-8
 #Программа для просмотра файлов prg в заданном каталоге
 #поддержка файлов формата prg v2, v1
-#Увеличение/уменьшение - крутить колесо мыши
-#TODO - первый запуск на tkinter
+#27/03/2014  вместо кнопки Show/Hide флажки
+#TODO заменить кнопки на список
+#TODO кнопка для сброса фильтра
 
+from sys import argv as sys_argv
 from os import path as path2program
 from os import listdir
 from os import _exit
+from os import chdir
 
 from tkinter import Tk
 from tkinter import Canvas
-from tkinter import Event, StringVar, IntVar
+from tkinter import Event, StringVar, IntVar, BooleanVar
 from tkinter import Menu, Button, Scale, Radiobutton, Frame, Label
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showinfo
@@ -28,8 +31,11 @@ class prgGroup(object):
         self.group = None #исходные данные
         self.primitives = None #группа примитивов
         self.descriptions = None #группа описаний
-        #self.form = "rectangle"
+        self.form = "rectangle"
+        self.jakors = ["ALL"]
+        
     
+
 class prgCanvas(object):
     def __init__(self,window,height,width):
         self.canvas = Canvas(window, bg="grey", height=height, width=width)
@@ -38,19 +44,18 @@ class prgCanvas(object):
         self.mashtab = [1.00,1.00]
         self.reper = [0.00,0.00]
         self.sXY = [0.00,0.00]
-        
+
         self.field = prgGroup() #данные, блоки групп, графические примитивы, точки, #графические примитивы, надписи
         self.fileprogram = None #имя файла для загрузки
-        
+
         self.filter = None #фильтр, по которому определяется выделенный фрагмент
-        
-    def configure(self,file,filter= None):
-        self.fileprogram = file
+
+    def configure(self,file=None,filter= None):
+        if file!=None:
+            self.fileprogram = file
         if filter!=None:
             self.filter=filter
-        else:
-            self.filter = lambda x: x=="1999"#тестовый
-    
+
     def setMashtab(self):
         '''Эта функция устанавливает текущий масштаб, так чтобы вся плата была в зоне видимости'''
         def findcoord(field):
@@ -71,19 +76,13 @@ class prgCanvas(object):
         cY = lY/lengthY
         print("MSH",cX,cY)
         self.mashtab = [cX,cY]
-        
+
     def genfield(self):
         x,y,d=0,1,2
-        #нужно научиться обрабатывать группу
-        if self.field.primitives != None:
-            for c in self.field.primitives:
-                self.canvas.delete(c)
-        if self.field.descriptions != None:
-            for c in self.field.descriptions:
-                self.canvas.delete(c)
-        self.field.primitives = list()
-        self.field.descriptions = list()
         
+        self.canvas.delete("FIG")
+        self.canvas.delete("DESC")
+
         for c in self.field.group:
             cx = (c[x]+self.sXY[x])*self.mashtab[x]+self.reper[x]
             cy = (c[y]+self.sXY[y])*self.mashtab[y]+self.reper[y]
@@ -92,22 +91,18 @@ class prgCanvas(object):
                 _color1,_color2 = ["red","red"] if self.filter(c[d][2]) else ["black","black"]
             else:
                 _color1,_color2 = "black","black"
-            #
-            self.field.primitives.append(self.canvas.create_rectangle(cx-1,cy-1,cx+1,cy+1,outline=_color1,fill=_color2))
+            
+            self.canvas.create_rectangle(cx-1,cy-1,cx+1,cy+1,outline=_color1,fill=_color2,tag="FIG")
             if self.flagDescription:
-                self.field.descriptions.append(self.canvas.create_text(cx,cy,anchor="nw",text=str(c[d][1]), fill=_color1,font="Verdana 8"))
+                self.canvas.create_text(cx,cy,anchor="nw",text=str(c[d][1]), fill=_color1,font="Verdana 8",tag="DESC")
 
-    def move(self,event,x,y):
+    def move(self,x,y):
         #в группы
         self.reper[0]+=x
         self.reper[1]+=y
-        if self.field.primitives != None:
-            for c in self.field.primitives:
-                self.canvas.move(c,x,y)
-        if (self.field.descriptions != None) and (self.flagDescription):
-            for c in self.field.descriptions:
-                self.canvas.move(c,x,y)
-    
+        self.canvas.move("FIG",x,y)
+        self.canvas.move("DESC",x,y)
+
     def load(self):
         _p = prg(self.fileprogram)
         _p.download()
@@ -115,16 +110,15 @@ class prgCanvas(object):
         #здесь можно разместить фильтрацию по группам, только фильтр(фильтры) нужно передавать через параметр
         self.field.group = [x[1:4] for x in _p.progdigit if "25" in x[3]]
         print(self.field)
-        
+
     def paint(self):
         self.load()
-        self.setMashtab()
-        self.genfield()
-    
-class stringsApp(object):
-    def __init__(self):
-        self.currentfile="Current file"
-        
+        try:
+            self.setMashtab()
+            self.genfield()
+        except ZeroDivisionError:
+            print("Zero division")
+
 class App(object):
     def __init__(self):
         #настройка окружения
@@ -140,6 +134,10 @@ class App(object):
         self.window.title("PRG Viewer by Novicov: "+self.pathtoapp)
         self.window.minsize(width=510,height=510)
         self.window.maxsize(width=800,height=800)
+        #иконка
+        _lst = sys_argv[0].split('\\')
+        self.window.iconbitmap('\\'.join(_lst[:-1])+'\\PRGViewer-logo.ico')
+        
         self.rightframe = Frame(self.window, bg="light grey",height=500, width=200)
         self.inframe = Frame(self.rightframe, bg="yellow")
         self.lstButton = None
@@ -147,20 +145,52 @@ class App(object):
         self.canvas = prgCanvas(self.window,500,500)
         self.canvas.configure(file=self.lst[self.currentfileindex])
         self.canvas.paint()
-        
-        
-        self.showbutton = Button(self.rightframe,text="Hide descriptions",width=20)
+
+        self.showDescriptions = BooleanVar()
+        self.showDescriptions.set(1)
+        self.rbDescShow = Radiobutton(self.rightframe, text = "Hide description",width=20,
+                                      variable = self.showDescriptions, value = 0)
+        self.rbDescHide = Radiobutton(self.rightframe, text = "Show description",width=20,
+                                      variable = self.showDescriptions, value = 1)
+
         self.infoText = StringVar()
         self.infoText.set("Current file: "+self.lst[self.currentfileindex])
         self.info = Label(self.rightframe,text=self.infoText.get())
-        self.listfiles = Label(self.rightframe,text="\n".join(self.lst))
+        self.listFilesText = StringVar()
+        self.listFilesText.set("\n".join(self.lst))
+        self.listfiles = Label(self.rightframe,text=self.listFilesText.get())
         self.lmashtab = Label(self.rightframe, text=":".join([str(x) for x in self.canvas.mashtab]))
         self.helpText = Label(self.rightframe, text="Use mouse wheel to select file\n"+
             "Use left mouse button to load file\n"+
             "Use Up,Down,Right,Left buttons to move field\n"+
             "Use Show/Hide descriptions button\n"+
             "Use +/- buttons to change scale of field",anchor="n",justify=LEFT)
-        
+
+    def set_path_and_current(self, filename):
+        '''
+        эта функция обрабатывает полный путь до файла
+        '''
+        try:
+            _lst = filename.split('\\')
+            self.path    = '\\'.join(_lst[:-2])
+            chdir('\\'.join(_lst[:-1]))
+            print(listdir("."))
+            self.lst     = [x for x in listdir(".")  if ".prg" in x]
+            self.currentfileindex = self.lst.index(_lst[-1])
+            
+            self.infoText.set("Current file: "+self.lst[self.currentfileindex])
+            self.info.configure(text=self.infoText.get())
+            
+            self.listFilesText.set("\n".join(self.lst))
+            self.listfiles.configure(text=self.listFilesText.get())
+            
+            self.canvas.configure(self.lst[self.currentfileindex])
+            self._genAll()
+        except IOError:
+            self.infoText.set("Error")
+            self.info.configure(text=self.infoText.get())
+            
+
     def _wheel(self,event):
         """
         реакция на прокрутку колеса мыши
@@ -172,32 +202,36 @@ class App(object):
             self.currentfileindex = len(self.lst)-1
         elif self.currentfileindex>len(self.lst)-1:
             self.currentfileindex = 0
-            
+
         self.canvas.reper = [0.00,0.00]
         self.infoText.set("Current file: "+self.lst[self.currentfileindex])
         self.info.configure(text=self.infoText.get())
         self.canvas.configure(self.lst[self.currentfileindex])
-   
+
     def _hideshowdescriptions(self,event):
-        if self.canvas.flagDescription:
-            txt = "Show descriptions"
-        else:
-            txt = "Hide descriptions"
-        self.canvas.flagDescription = not self.canvas.flagDescription
-        self.showbutton["text"] = txt
-        # self.canvas.paint()
+        '''
+        '''
+        self.canvas.flagDescription = self.showDescriptions.get()
         self.canvas.genfield()
-        
+
     def _changemashtab(self,event,dmX,dmY):
         x,y=0,1
         self.canvas.mashtab[x]*=dmX
         self.canvas.mashtab[y]*=dmY
         self.canvas.genfield()
-        
-    def _genAll(self,event):
+
+    def _selectGroup(self,name):
+        print("select"+name)
+        #устанавливаем фильтр
+        self.canvas.configure(filter=lambda x: x==name)
+        #перерисовываем
+        self.canvas.paint()
+
+    def _genAll(self):
         '''
         сгенерировать список кнопок, удалить перед использованием
         '''
+        #рисуем
         self.canvas.paint()
         #здесь мы создаем группу кнопок
         gr = list()
@@ -209,26 +243,31 @@ class App(object):
             for item in self.lstButton:
                 item.destroy()
         self.lstButton = list()
+
+        fabric = lambda func,par: lambda: func(par)
         for g in gr:
-            b = Button(self.inframe,text=g)
+            b = Button(self.inframe,text=g, command = fabric(self._selectGroup,g))
             b.pack()
             self.lstButton.append(b)
-        
-    
+
+
     def configure(self):
         self.window.bind("<MouseWheel>",lambda event:self._wheel(event))
-        self.canvas.canvas.bind("<Button-1>",lambda event:self._genAll(event))
-        self.window.bind("<Key-Right>",lambda event:self.canvas.move(event,10,0)) 
-        self.window.bind("<Key-Left>",lambda event:self.canvas.move(event,-10,0))
-        self.window.bind("<Key-Down>",lambda event:self.canvas.move(event,0,10))
-        self.window.bind("<Key-Up>",lambda event:self.canvas.move(event,0,-10))
+        self.canvas.canvas.bind("<Button-1>",lambda event:self._genAll())
+        self.window.bind("<Key-Right>",lambda event:self.canvas.move(10,0))
+        self.window.bind("<Key-Left>",lambda event:self.canvas.move(-10,0))
+        self.window.bind("<Key-Down>",lambda event:self.canvas.move(0,10))
+        self.window.bind("<Key-Up>",lambda event:self.canvas.move(0,-10))
         self.window.bind("<Key-plus>",lambda event:self._changemashtab(event,1.1,1.1))
+        self.window.bind("p",lambda event:self._changemashtab(event,1.1,1.1))
         self.window.bind("<Key-minus>",lambda event:self._changemashtab(event,0.9,0.9))
-        self.showbutton.bind("<Button-1>",lambda event:self._hideshowdescriptions(event))
-        
+        self.window.bind("m",lambda event:self._changemashtab(event,0.9,0.9))
+        self.trace_show = self.showDescriptions.trace_variable("w",lambda v,i,m:self._hideshowdescriptions(v))
+
     def startloop(self):
         self.rightframe.pack(side="right",expand="y",fill="y")
-        self.showbutton.pack(side="top")
+        self.rbDescHide.pack(side="top")
+        self.rbDescShow.pack(side="top")
         self.info.pack(side="top")
         self.listfiles.pack(side="top")
         self.lmashtab.pack(side="top")
@@ -238,5 +277,7 @@ class App(object):
 
 if __name__ == "__main__":
     PRGviewerapp = App()
+    if (len(sys_argv)==2):
+        PRGviewerapp.set_path_and_current(sys_argv[1])
     PRGviewerapp.configure()
     PRGviewerapp.startloop()
