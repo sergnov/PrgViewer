02cafd20a3235fb62590b0f90c5050f020582d38
S0nov <zkgoldenman@gmail.com>
2014-05-31 23:11:00 +0400

    update setup

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ WIN_Target = cxExecutable(script = "PRGViewer.py",
     icon = "PRGViewer-logo.ico")
     
 setup(  name = "PRGViewer",        
-    version = "2.3.0",
-    description = "PRGViewer by Novicov 2.3.0",
+    version = "2.3.5",
+    description = "PRGViewer by Novicov 2.3.5",
     options = {"build_exe": build_exe_options},        
     executables = [WIN_Target])
